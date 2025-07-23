bd393c370a082aa33b9d88204c3d7ba0ff545836
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-07-24 02:52:40 +0600

    mongoDb,backend,frontend connected

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,12 @@ const app = express();
 // All Routes:
 const empRoute = require("./routes/employeeRoute");
 
+const MONGO_URI = `mongodb+srv://<db_username>:<db_password>@cluster0.m0cbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error(err));
+
 app.use(express.json());
 app.use("/api/emp", empRoute);
 
--- Frontend/src/components/EmployeeManagement.jsx
+++ Frontend/src/components/EmployeeManagement.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 const EmployeeManagement = () => {
+
+    const [empId, setEmpId] = useState('');
+    const [empName, setEmpName] = useState('');
+    const [empSkills, setEmpSkills] = useState('');
+
+    const handleAddEmployee = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('http://localhost:3000/api/emp/createEmp', { empId, empName, empSkills });
+            setEmpId('');
+            setEmpName('');
+            setEmpSkills('');
+            console.log('Employee added successfully', res.data);
+        } catch (error) {
+            console.log('Error adding employee:', error);
+        }
+    }
+
     return (
         <div className='emp-wrapper text-violet-500 bg-gray-200 shadow-md rounded p-5 w-1/3'>
             <h2 className='text-3xl text-center font-semibold mb-5'>Add Employee</h2>
@@ -9,25 +29,35 @@ const EmployeeManagement = () => {
                 <label className='block mb-2'>Enter Employee ID</label>
                 <input type="text"
                     placeholder='Enter Employee ID'
-                    className='border rounded w-full p-2' />
+                    className='border rounded w-full p-2'
+                    value={empId}
+                    onChange={(e) => setEmpId(e.target.value)}
+                />
             </div>
 
             <div className='input-group mb-4'>
                 <label className='block mb-2'>Enter Employee Name</label>
                 <input type="text"
                     placeholder='Enter Employee Name'
-                    className='border rounded w-full p-2' />
+                    className='border rounded w-full p-2'
+                    value={empName}
+                    onChange={(e) => setEmpName(e.target.value)}
+                />
             </div>
 
             <div className='input-group mb-4'>
                 <label className='block mb-2'>Enter Employee Skills</label>
                 <input type="text"
                     placeholder='Enter Employee Skills'
-                    className='border rounded w-full p-2' />
+                    className='border rounded w-full p-2'
+                    value={empSkills}
+                    onChange={(e) => setEmpSkills(e.target.value)}
+                />
             </div>
 
             <div className='btn-group text-center'>
-                <button className='w-1/2 bg-violet-500 text-white py-3 rounded'>
+                <button className='w-1/2 bg-violet-500 text-white py-3 rounded'
+                    onClick={handleAddEmployee}>
                     Add New Employee
                 </button>
             </div>
